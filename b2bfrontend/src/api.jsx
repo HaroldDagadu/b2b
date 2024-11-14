ad996e5f608d8afba67c4831ee308ee35d811447
Harold Dagadu <dagaduharoldhkd@gmail.com>
2024-11-14 14:38:16 +0000

    b2b

--- b2bfrontend/src/api.jsx
+++ b2bfrontend/src/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the base URL set to port 8000
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://35.226.26.69',
   withCredentials: true, // Enable cookies for cross-site access if needed
 
 });
